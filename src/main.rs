67e27c81145f785e55c862c68e08d4b835a0ce45
Cameron Eldridge <cameldridge+git@gmail.com>
2018-07-05 20:09:12 -0400

    Some structure building

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #![feature(macro_at_most_once_rep)]
 
 #[macro_use] extern crate game_engine as engine;
+#[macro_use] extern crate serde_derive;
+extern crate serde;
 
+mod component;
+mod entity;
+mod resource;
 mod scene;
 
 use engine::Game;
