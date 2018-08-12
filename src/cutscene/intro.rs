d518e01cd4f102d93b1f74496e4d429c9bb4305e
Cameron Eldridge <cameldridge+git@gmail.com>
2018-08-12 15:12:59 -0400

    remove unneeded breaks

--- src/cutscene/intro.rs
+++ src/cutscene/intro.rs
@@ -14,19 +14,16 @@ enum EnterAlleyEvent {
 pub fn enter_alley() -> Box<dyn Cutscene> {
     StandardCutscene::boxed(&[
         Move(Id::Player, &[Point::new(TILE_SIZE as f32 * 24f32, TILE_SIZE as f32 * 11f32)]),
-        Break,
         AwaitMoveEnd(Id::Player),
         StartDialog(dialog::enter_alley::story),
         AwaitDialogEvent(EnterAlleyEvent::ComeOut),
         Move(Id::MysteryMan, &[Point::new(TILE_SIZE as f32 * 19f32, TILE_SIZE as f32 * 11f32)]),
-        Break,
         AwaitDialogEnd,
         Move(Id::Player, &[Point::new(TILE_SIZE as f32 * 25f32, TILE_SIZE as f32 * 11f32)]),
         Move(Id::MysteryMan, &[
             Point::new(TILE_SIZE as f32 * 24f32, TILE_SIZE as f32 * 11f32),
             Point::new(TILE_SIZE as f32 * 24f32, TILE_SIZE as f32 * 5f32),
         ]),
-        Break,
         AwaitMoveEnd(Id::MysteryMan),
         StateChange(MainState::HideSomewhere),
     ])
